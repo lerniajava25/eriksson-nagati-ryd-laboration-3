a02014beb0e924f1651fae6e6e12c554f02e818b
Anders Eriksson <anteman.swe@gmail.com>
2026-09-08 18:19:27 +0200

    Add WarehouseExampleTest and update test dependencies in pom.xml

---
+++ src/test/java/test/WarehouseExampleTest.java
@@ -0,0 +1,13 @@
+package test;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class WarehouseExampleTest {
+
+    @Test
+    void testWarehouse() {
+        assertThat(1).isEqualTo(1);
+    }
+}
