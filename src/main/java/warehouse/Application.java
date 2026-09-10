b1f5630291dd6b296d91e5c120a4e4e2594954a0
Anders Eriksson <anteman.swe@gmail.com>
2026-09-10 14:24:24 +0200

    Add maven-surefire-plugin and configure mockito; cleanup Application.java

--- src/main/java/warehouse/Application.java
+++ src/main/java/warehouse/Application.java
@@ -7,18 +7,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @SpringBootApplication
 public class Application {
-
-    @GetMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
-    @PostMapping("/")
-
-    @PutMapping("/")
-
-    @DeleteMapping("/")
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
